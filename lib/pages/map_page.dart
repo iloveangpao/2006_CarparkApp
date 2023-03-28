e29fd51b74d825c2800cddda2093fbd9d8e0b995
Rhys Wong <115345544+Rhys-Wong@users.noreply.github.com>
2023-03-28 19:46:45 +0800

    Fixed Map

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,182 +1,146 @@
-import 'dart:async';
-
+import '../classes/carpark.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:geolocator_platform_interface/src/enums/location_permission.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
-import 'currentBooking.dart';
-import './booking_page.dart';
-import './chatPage.dart';
-import './favPage.dart';
-import './SearchPageArea.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-// void main() => runApp(MaterialApp(
-//       home: MapSample(),
-//     ));
 
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       bottomNavigationBar: BottomNavigationBar(
-//           backgroundColor: Colors.blue[900],
-//           unselectedItemColor: Colors.white,
-//           type: BottomNavigationBarType.fixed,
-//           unselectedLabelStyle:
-//               const TextStyle(color: Colors.white, fontSize: 14),
-//           items: const [
-//             BottomNavigationBarItem(
-//               icon: Icon(Icons.home, color: Colors.white),
-//               label: 'Search',
-//             ),
-//             BottomNavigationBarItem(
-//                 icon: Icon(Icons.search, color: Colors.white),
-//                 label: 'Bookings'),
-//             BottomNavigationBarItem(
-//                 icon: Icon(Icons.heart_broken_rounded, color: Colors.white),
-//                 label: 'Favourites'),
-//             BottomNavigationBarItem(
-//                 icon: Icon(Icons.phone_in_talk, color: Colors.white),
-//                 label: 'Settings'),
-//           ]),
-//     );
-//   }
-// }
 
-class MapPage extends StatefulWidget {
-  const MapPage({Key? key}) : super(key: key);
-
-  @override
-  State<MapPage> createState() => MapPageState();
+void main(){
+  runApp(MapPage());
 }
 
-class MapPageState extends State<MapPage> {
-  int _selectedIndex = 0;
-  final screens = [
-    SearchPageArea(),
-    currentBookingPage(),
-    FavPage(),
-    ChatPage()
-  ];
+class MapPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Google Map",
+        debugShowCheckedModeBanner: false,
+        theme:ThemeData(
+          primaryColor: Colors.white,
+        ),
+        home:MapScreen()
+    );
+  }
+}
 
-  void _onItemTapped(int index) {
+class MapScreen extends StatefulWidget {
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  List<carpark> _carpark = <carpark>[];
+
+  Future<List<carpark>> fetchcarpark() async {
+    final response = await http.get(Uri.parse('http://20.187.121.122/avail/'));
+
+    var carparks = <carpark>[];
+
+    if (response.statusCode == 200) {
+
+      var carparksJson = json.decode(response.body);
+
+      for (var carparkJson in carparksJson) {
+        carparks.add(carpark.fromJson(carparkJson));
+      }
+      setState(() {
+        _carparks = carparks; // Update the carparks list with the fetched carparks
+      });
+    }
+    return carparks;
+  }
+
+  Set<Marker> _createMarkers() {
+    return _carparks.map((carpark) {
+      print("Hello");
+      print(carpark.latitude);// print statement here
+      return Marker(
+        markerId: MarkerId(carpark.carparkNo),
+        position: LatLng(carpark.latitude,carpark.longitude),
+        icon: BitmapDescriptor.defaultMarker,
+        infoWindow: InfoWindow(
+          title: carpark.carparkNo,
+          snippet: '${carpark.lotsAvailable} lots available',
+        ),
+      );
+    }).toSet();
+  }
+
+
+  static final Marker _testmarker = Marker(
+      markerId: MarkerId("_test"),
+      infoWindow: InfoWindow(title: "testmarker"),
+      icon: BitmapDescriptor.defaultMarker,
+      position: LatLng(1.31827878141546,103.87742961097146)
+  );
+
+  LatLng? _currentLocation;
+
+  void _getCurrentLocation() async {
+    final Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
     setState(() {
-      _selectedIndex = index;
+      _currentLocation = LatLng(position.latitude, position.longitude);
     });
   }
 
-  final Completer<GoogleMapController> _controller =
-      Completer<GoogleMapController>();
-
-  CameraPosition _initialPosition = CameraPosition(target: LatLng(0, 0));
-
-  Future<LatLng> getCurrentLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high,
-    );
-    return LatLng(position.latitude, position.longitude);
-  }
-
-  static const CameraPosition _woodlandsmrt = CameraPosition(
-    target: LatLng(1.4368967, 103.78658),
-    zoom: 14.4746,
-  );
-
-  static const CameraPosition _marsiling = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(1.43239, 103.7741167),
-      zoom: 14);
-
-  static final Marker _marsilingmarker = Marker(
-    markerId: MarkerId("_kGooglePlexMarker"),
-    infoWindow: InfoWindow(title: "Marker"),
-    icon: BitmapDescriptor.defaultMarker,
-    position: LatLng(1.43239, 103.7741167),
-  );
-
-  static Marker _woodlandsmrtmarker = Marker(
-    markerId: MarkerId("_kSecondMarker"),
-    infoWindow: InfoWindow(title: "Second"),
-    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
-    position: LatLng(1.4368967, 103.78658),
-  );
-
-  //static const LatLng source =
-
-  List<LatLng> polyLineCoordinates = [];
-
-  void getPolyPoints() async {
-    String googleApiKey = "AIzaSyCOsATiiB_VhRpYdmUHhnkf48BcfokDJ-s";
-    PolylinePoints polylinePoints = PolylinePoints();
-    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
-        googleApiKey,
-        PointLatLng(_marsiling.target.latitude, _marsiling.target.longitude),
-        PointLatLng(
-            _woodlandsmrt.target.latitude, _woodlandsmrt.target.longitude));
-
-    if (result.points.isNotEmpty) {
-      result.points.forEach((PointLatLng point) =>
-          polyLineCoordinates.add(LatLng(point.latitude, point.longitude)));
-      setState(() {});
-    }
-  }
 
   @override
   void initState() {
-    getPolyPoints();
     super.initState();
+    fetchcarpark().then((carparks) {
+      setState(() {
+        _carparks = carparks;
+      });
+    });
+    _getCurrentLocation();
   }
+
+
+  static const _initialCameraPosition = CameraPosition(
+    target: LatLng(1.31827878141546,103.87742961097146),
+    zoom: 20.5,
+  );
+
+  late GoogleMapController _googleMapController;
+  List<carpark> _carparks = [];
+
+  @override
+  void dispose() {
+    _googleMapController.dispose();
+    super.dispose();
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-        mapType: MapType.normal,
-        markers: {_woodlandsmrtmarker, _marsilingmarker},
-        polylines: {
-          Polyline(
-            polylineId: PolylineId("route"),
-            points: polyLineCoordinates,
-          )
-        },
-        initialCameraPosition: _marsiling,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+          myLocationButtonEnabled: false,
+          zoomGesturesEnabled: false,
+          initialCameraPosition: _currentLocation == null
+              ? _initialCameraPosition
+              : CameraPosition(target: _currentLocation!, zoom: 15),
+          onMapCreated: (controller) => _googleMapController = controller,
+          markers: _createMarkers()
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _goToCurrentLoc,
-        backgroundColor: Colors.white,
-        child: Icon(
-          Icons.my_location,
-          color: Colors.grey,
-        ),
-      ),
-    );
-  }
-
-  Future<void> _goToCurrentLoc() async {
-    final GoogleMapController controller = await _controller.future;
-    Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high,
-    );
-    LatLng userLocation = LatLng(position.latitude, position.longitude);
-    Marker updatedMarker = _woodlandsmrtmarker.copyWith(
-      positionParam: userLocation,
-    );
-
-    setState(() {
-      _woodlandsmrtmarker = updatedMarker;
-      print(_woodlandsmrtmarker.position);
-    });
-    controller.animateCamera(
-      CameraUpdate.newCameraPosition(
-        CameraPosition(
-          target: userLocation,
-          zoom: 14.0,
-        ),
+        backgroundColor: Theme.of(context).primaryColor,
+        foregroundColor: Colors.black,
+        onPressed: () {
+          if (_currentLocation != null) {
+            _googleMapController.animateCamera(
+              CameraUpdate.newCameraPosition(
+                CameraPosition(target: _currentLocation!, zoom: 15),
+              ),
+            );
+          }
+        },
+        child: const Icon(Icons.center_focus_strong),
       ),
     );
   }
